f070d5bc5439a62148d066225b1922cf676093c1
Tarik <bazhenkataras@gmail.com>
2021-09-18 20:14:03 +0300

    Add new data in css html

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+let a = document.querySelector('.navigation');
+let gambBtn = a.querySelector('.navigation_wrapper_gamburger ');
+let menu = a.querySelector('.navigation_wrapper_gamburger-menu');
+   
+const btn= gambBtn.addEventListener ('click', () => {
+menu.classList.toggle('active');
+
+});   
+
+
+
+
+  
